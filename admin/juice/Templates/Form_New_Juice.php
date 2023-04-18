40f06e95f6d561b0e0efe32383818b8b5bec2e8c
RaphaelMarchette <99852899+RaphaelMarchette@users.noreply.github.com>
2023-04-18 16:51:27 -0300

    UYT

--- admin/juice/Templates/Form_New_Juice.php
+++ admin/juice/Templates/Form_New_Juice.php
@@ -26,10 +26,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="../../Static_18/css/Tela.css"/>
-    <link rel="stylesheet" href="../../Static_18/css/Table.css"/>
-    <link rel="stylesheet" href="../../Static_18/css/Form_Create_Juice.css" />
-    <script src="../../Static/Scripts/View.js" defer></script>
+    <link rel="stylesheet" href="../Static_19/css/Tela.css"/>
+    <link rel="stylesheet" href="../Static_19/css/Table.css"/>
+    <link rel="stylesheet" href="../Static_19/css/Form_Create_Juice.css" />
+    <script src="../Static_19/Scripts/View.js" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <title>Juice Max | New Juice</title>
 </head>
